5ee0ddd9b3008db8dbd599d2d074f6aba13af904
Enrico Degregori <degregori@dkrz.de>
2024-06-02 11:25:45 +0200

    dev: open/close library and initialization/finalization functions

--- src/DistDir.jl
+++ src/DistDir.jl
@@ -1,5 +1,26 @@
 module DistDir
 
-# Write your package code here.
+using Libdl
+
+export initialize
+
+const libdistdir = "libdistdir"
+libdistdir_handle = C_NULL
+
+function initialize()
+	@ccall libdistdir.distdir_initialize()::Cvoid
+end
+
+function finalize()
+	@ccall libdistdir.distdir_finalize()::Cvoid
+	Libdl.dlclose(libdistdir_handle)
+	println("libdistdir successfully closed !")
+end
+
+function __init__()
+	libdistdir_handle = Libdl.dlopen(libdistdir, Libdl.RTLD_LAZY | Libdl.RTLD_GLOBAL)
+
+	println("libdistdir successfully opened !")
+end
 
 end

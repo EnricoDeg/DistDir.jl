c8260700bdc485008dea959f42dde9a748690c68
Enrico Degregori <degregori@dkrz.de>
2024-06-05 23:04:46 +0200

    devs: try using pointer to void instead of replicating C structs
    It seems to work fine in this case, but it needs to be tested
    also with t_map and t_exchanger

--- src/idxlist.jl
+++ src/idxlist.jl
@@ -1,22 +1,17 @@
 include("system.jl")
 
-struct t_idxlist
-	count::Cint
-	list::Ptr{Cint}
-end
-
 struct t_idxlist_jl
-	cptr::Ptr{t_idxlist}
+	cptr::Ptr{Cvoid}
 end
 
 function new_idxlist(list :: Vector{Int32})
-	return t_idxlist_jl( @ccall libdistdir.new_idxlist(list :: Ptr{Cint}, length(list) :: Cint)::Ptr{t_idxlist} )
+	return t_idxlist_jl( @ccall libdistdir.new_idxlist(list :: Ptr{Cint}, length(list) :: Cint)::Ptr{Cvoid} )
 end
 
 function new_idxlist()
-	return t_idxlist_jl( @ccall libdistdir.new_idxlist_empty()::Ptr{t_idxlist} )
+	return t_idxlist_jl( @ccall libdistdir.new_idxlist_empty()::Ptr{Cvoid} )
 end
 
 function delete_idxlist(idxlist::t_idxlist_jl)
-	@ccall libdistdir.delete_idxlist(idxlist.cptr::Ptr{t_idxlist})::Cvoid
+	@ccall libdistdir.delete_idxlist(idxlist.cptr::Ptr{Cvoid})::Cvoid
 end
